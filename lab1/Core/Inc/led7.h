ad9cb6702a15a3ecc02bb2a9848cc0dacbe8eaea
mimin514 <my.nguyen2004@hcmut.edu.vn>
2024-09-12 22:51:24 +0700

    ex5 ok( proteus mới)

--- lab1/Core/Inc/led7.h
+++ lab1/Core/Inc/led7.h
@@ -9,9 +9,12 @@
 #define INC_LED7_H_
 #include "main.h"
 
-void display(int num);
+void display1(int num);
 void display2(int num);
 
 void display1to9(void);
-void display2_1to9(void);
+void display_2_1to9(void);
+
+void display1_run(void);
+void display2_run(void);
 #endif /* INC_LED7_H_ */
--- lab1/Core/Inc/main.h
+++ lab1/Core/Inc/main.h
@@ -57,10 +57,6 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define LED_RED_Pin GPIO_PIN_5
-#define LED_RED_GPIO_Port GPIOA
-#define LED_YELLOW_Pin GPIO_PIN_6
-#define LED_YELLOW_GPIO_Port GPIOA
 #define SEG_0_Pin GPIO_PIN_7
 #define SEG_0_GPIO_Port GPIOA
 #define LED_1_Pin GPIO_PIN_0
@@ -69,10 +65,18 @@ void Error_Handler(void);
 #define LED_2_GPIO_Port GPIOB
 #define LED_3_Pin GPIO_PIN_2
 #define LED_3_GPIO_Port GPIOB
-#define LED_11_Pin GPIO_PIN_10
-#define LED_11_GPIO_Port GPIOB
-#define LED_12_Pin GPIO_PIN_11
-#define LED_12_GPIO_Port GPIOB
+#define SEG_11_Pin GPIO_PIN_10
+#define SEG_11_GPIO_Port GPIOB
+#define SEG_22_Pin GPIO_PIN_11
+#define SEG_22_GPIO_Port GPIOB
+#define SEG_33_Pin GPIO_PIN_12
+#define SEG_33_GPIO_Port GPIOB
+#define SEG_44_Pin GPIO_PIN_13
+#define SEG_44_GPIO_Port GPIOB
+#define SEG_55_Pin GPIO_PIN_14
+#define SEG_55_GPIO_Port GPIOB
+#define SEG_66_Pin GPIO_PIN_15
+#define SEG_66_GPIO_Port GPIOB
 #define SEG_1_Pin GPIO_PIN_8
 #define SEG_1_GPIO_Port GPIOA
 #define SEG_2_Pin GPIO_PIN_9
@@ -85,20 +89,18 @@ void Error_Handler(void);
 #define SEG_5_GPIO_Port GPIOA
 #define SEG_6_Pin GPIO_PIN_13
 #define SEG_6_GPIO_Port GPIOA
+#define SEG_11A14_Pin GPIO_PIN_14
+#define SEG_11A14_GPIO_Port GPIOA
+#define SEG_22A15_Pin GPIO_PIN_15
+#define SEG_22A15_GPIO_Port GPIOA
 #define LED_4_Pin GPIO_PIN_3
 #define LED_4_GPIO_Port GPIOB
 #define LED_5_Pin GPIO_PIN_4
 #define LED_5_GPIO_Port GPIOB
 #define LED_6_Pin GPIO_PIN_5
 #define LED_6_GPIO_Port GPIOB
-#define LED_7_Pin GPIO_PIN_6
-#define LED_7_GPIO_Port GPIOB
-#define LED_8_Pin GPIO_PIN_7
-#define LED_8_GPIO_Port GPIOB
-#define LED_9_Pin GPIO_PIN_8
-#define LED_9_GPIO_Port GPIOB
-#define LED_10_Pin GPIO_PIN_9
-#define LED_10_GPIO_Port GPIOB
+#define SEG_00_Pin GPIO_PIN_9
+#define SEG_00_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
